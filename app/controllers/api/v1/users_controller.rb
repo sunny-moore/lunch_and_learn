dba5957092623e77b9559d9492b9a0cf88a2a7d4
Sunny Moore <moore.sunny@gmail.com>
2022-11-15 16:28:31 -0800

    Set up user table, controller and serialized response

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: UserSerializer.new(user), status: 201
+    elsif User.find_by(email: params[:email])
+      render json: {error: 'This email already exists'}, status: 400
+    else
+      render json: {error: 'An unknown error occured'}, status: 418
+    end
+  end
+
+private
+
+  def user_params
+    params.permit(:name, :email, :api_key)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  before_create { self.api_key = secure_key }
+
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  # validates_presence_of :api_key
+
+  def secure_key
+    SecureRandom.hex(15)
+  end
+end
